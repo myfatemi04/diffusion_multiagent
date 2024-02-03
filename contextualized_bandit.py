9053c27feacfc5cc215191551f206d080a26dd63
Michael Fatemi <myfatemi04@gmail.com>
2024-02-03 11:43:41 -0500

    some sample code for contextualized bandit

--- contextualized_bandit.py
+++ contextualized_bandit.py
@@ -13,8 +13,10 @@ How do we define collaborative tasks?
     imagine one of the agents providing supplies to the other.
 """
 
+from typing import List, Tuple
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch_geometric.nn as gnn
 from torch_geometric.data import HeteroData
 import numpy as np
@@ -62,11 +64,16 @@ class GNN(nn.Module):
         self.lin1 = gnn.Linear(-1, hidden_channels)
         self.conv2 = gnn.SAGEConv(hidden_channels, out_channels, dropout=0.1)
         self.lin2 = gnn.Linear(hidden_channels, out_channels)
+        self.layernorm = gnn.LayerNorm(out_channels)
+        self.hidden_channels = hidden_channels
+        self.out_channels = out_channels
 
     def forward(self, x, edge_index):
         x = self.conv1(x, edge_index) + self.lin1(x)
         x = x.relu()
         x = self.conv2(x, edge_index) + self.lin2(x)
+        # so the task and policy embeddings are reasonable
+        x = self.layernorm(x)
         return x
 
 def one_hot_2d(positions: torch.Tensor, width: int, height: int):
@@ -74,33 +81,73 @@ def one_hot_2d(positions: torch.Tensor, width: int, height: int):
     y_emb = nn.functional.one_hot(positions[..., 1], num_classes=height)
     return torch.concatenate([x_emb, y_emb], dim=-1)
 
+def create_heterodata(agent_locations, task_locations):
+    data = HeteroData()
+    data['agent'].x = one_hot_2d(torch.tensor(agent_locations, dtype=torch.long), width, height).float()
+    data['task'].x = one_hot_2d(torch.tensor(task_locations, dtype=torch.long), width, height).float()
+    # tasks <-> agents
+    data['agent', 'sees', 'task'].edge_index = torch.tensor([
+        [i for i in range(agent_locations.shape[0]) for j in range(task_locations.shape[0])],
+        [j for i in range(agent_locations.shape[0]) for j in range(task_locations.shape[0])]
+    ], dtype=torch.long)
+    data['task', 'sees', 'agent'].edge_index = torch.tensor([
+        [i for i in range(task_locations.shape[0]) for j in range(agent_locations.shape[0])],
+        [j for i in range(task_locations.shape[0]) for j in range(agent_locations.shape[0])]
+    ], dtype=torch.long)
+    # agent <-> agent, fully-connected
+    data['agent', 'sees', 'agent'].edge_index = torch.tensor([
+        [i for i in range(agent_locations.shape[0]) for j in range(agent_locations.shape[0])],
+        [j for i in range(agent_locations.shape[0]) for j in range(agent_locations.shape[0])]
+    ], dtype=torch.long)
+    return data
+
 # we will first use a contextualized bandit and make decisions with a gnn
 # will just use one-hot encoding for x and y positions
 width = 100
 height = 100
-agent_locations, task_locations, task_assignment = generate_scenario(10, 10, width, height)
-data = HeteroData()
-data['agent'].x = one_hot_2d(torch.tensor(agent_locations, dtype=torch.long), width, height).float()
-data['task'].x = one_hot_2d(torch.tensor(task_locations, dtype=torch.long), width, height).float()
-# tasks <-> agents
-data['agent', 'sees', 'task'].edge_index = torch.tensor([
-    [i for i in range(agent_locations.shape[0]) for j in range(task_locations.shape[0])],
-    [j for i in range(agent_locations.shape[0]) for j in range(task_locations.shape[0])]
-], dtype=torch.long)
-data['task', 'sees', 'agent'].edge_index = torch.tensor([
-    [i for i in range(task_locations.shape[0]) for j in range(agent_locations.shape[0])],
-    [j for i in range(task_locations.shape[0]) for j in range(agent_locations.shape[0])]
-], dtype=torch.long)
-# agent <-> agent, fully-connected
-data['agent', 'sees', 'agent'].edge_index = torch.tensor([
-    [i for i in range(agent_locations.shape[0]) for j in range(agent_locations.shape[0])],
-    [j for i in range(agent_locations.shape[0]) for j in range(agent_locations.shape[0])]
-], dtype=torch.long)
-print(data)
+n_scenarios = 100
+data: List[Tuple[HeteroData, np.ndarray]] = []
+
+for idx in range(n_scenarios):
+    agent_locations, task_locations, task_assignment = generate_scenario(10, 10, width, height)
+    data.append((create_heterodata(agent_locations, task_locations), task_assignment))
+
+dummy = data[0][0]
 
 net = GNN(64, 64)
-net = gnn.to_hetero(net, data.metadata(), aggr='sum')
+net = gnn.to_hetero(net, dummy.metadata(), aggr='sum')
 
-out = net(data.x_dict, data.edge_index_dict)
-# now we can see that the output is a dictionary with keys 'agent' and 'task'
-print(out.keys())
+# populate the channel sizes by passing in a dummy dataset of the same shape
+with torch.no_grad():
+    out = net(dummy.x_dict, dummy.edge_index_dict)
+    # now we can see that the output is a dictionary with keys 'agent' and 'task'
+    # print(out.keys())
+# used in CLIP, going to use it here
+logit_scale = torch.nn.Parameter(torch.tensor(1.0))
+
+optim = torch.optim.Adam([*net.parameters(), logit_scale], lr=1e-3, weight_decay=0.1)
+
+train = data[:int(0.8 * n_scenarios)]
+test = data[int(0.8 * n_scenarios):]
+
+for ep in range(10):
+    for (sample, task_assignment) in train:
+        out = net(sample.x_dict, sample.edge_index_dict)
+        # create assignment matrix
+        logits = (out['agent'] @ out['task'].T) * logit_scale
+        # apply cross-entropy loss. logits has shape [n_agents, n_tasks], and task_assignment has shape [n_agents]
+        loss = F.cross_entropy(logits, torch.tensor(task_assignment).long())
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+        print(loss.item())
+
+# eval
+with torch.no_grad():
+    for (sample, task_assignment) in test:
+        out = net(sample.x_dict, sample.edge_index_dict)
+        logits = (out['agent'] @ out['task'].T) * logit_scale
+        print(F.cross_entropy(logits, torch.tensor(task_assignment).long()).item())
+        print(logits.argmax(dim=1))
+        print(task_assignment)
+        print()
