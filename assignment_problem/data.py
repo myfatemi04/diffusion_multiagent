fix silly logprobs bug and add way to plot logprob evolution over time

assignment_problem/data.py
@@ -39,15 +39,18 @@ def one_hot_2d(positions: torch.Tensor, width: int, height: int):
     y_emb = nn.functional.one_hot(positions[..., 1], num_classes=height)
     return torch.concatenate([x_emb, y_emb], dim=-1)
 
-def create_heterodata(width, height, agent_locations, task_locations):
+def create_heterodata(width, height, agent_locations, task_locations, allow_agent_to_task=True):
     data = HeteroData()
     data['agent'].x = one_hot_2d(torch.tensor(agent_locations, dtype=torch.long), width, height).float()
     data['task'].x = one_hot_2d(torch.tensor(task_locations, dtype=torch.long), width, height).float()
     # tasks <-> agents
-    data['agent', 'sees', 'task'].edge_index = torch.tensor([
-        [i for i in range(agent_locations.shape[0]) for j in range(task_locations.shape[0])],
-        [j for i in range(agent_locations.shape[0]) for j in range(task_locations.shape[0])]
-    ], dtype=torch.long)
+    if allow_agent_to_task:
+        data['agent', 'sees', 'task'].edge_index = torch.tensor([
+            [i for i in range(agent_locations.shape[0]) for j in range(task_locations.shape[0])],
+            [j for i in range(agent_locations.shape[0]) for j in range(task_locations.shape[0])]
+        ], dtype=torch.long)
+    else:
+        data['agent', 'sees', 'task'].edge_index = torch.zeros((2, 0), dtype=torch.long)
     data['task', 'sees', 'agent'].edge_index = torch.tensor([
         [i for i in range(task_locations.shape[0]) for j in range(agent_locations.shape[0])],
         [j for i in range(task_locations.shape[0]) for j in range(agent_locations.shape[0])]

assignment_problem/reinforce_with_central_policy.py
@@ -13,6 +13,7 @@ import tqdm
 import visualize
 from data import generate_scenario, create_heterodata
 import reward_functions
+import sklearn.manifold
 
 # heterogenous graph learning: from https://pytorch-geometric.readthedocs.io/en/latest/notes/heterogeneous.html
 # we create a "heterognn"
@@ -22,23 +23,27 @@ class GNN(nn.Module):
         # these are `lazy`, input_channels=-1 are rederived at first forward() pass
         # and are automatically converted to use the correct message passing functions
         # with heterodata
+
+        # add an initial `projection` linear layer
+        self.initial_embed = gnn.Linear(-1, 64, bias=False)
+
         convs = []
         lins = []
         for i in range(len(channel_counts)):
             # convs.append(gnn.SAGEConv((-1, -1) if i == 0 else channel_counts[i - 1], channel_counts[i]))
             # lins.append(gnn.Linear(-1, channel_counts[i]))
             convs.append(gnn.GATConv((-1, -1) if i == 0 else channel_counts[i - 1], channel_counts[i], heads=1, dropout=0.1, add_self_loops=False))
-            lins.append(gnn.Linear(-1, channel_counts[i]))
+            lins.append(gnn.Linear(-1, channel_counts[i], bias=False))
         self.convs = nn.ModuleList(convs)
         self.lins = nn.ModuleList(lins)
         if len(channel_counts) > 0:
             self.layernorm = gnn.LayerNorm(channel_counts[-1])
-            self.projection = gnn.Linear(channel_counts[-1], 1)
         else:
             self.layernorm = None
-            self.projection = gnn.Linear(-1, 1)
+        self.projection = gnn.Linear(-1, 64, bias=False)
 
     def forward(self, x, edge_index):
+        x = self.initial_embed(x)
         for i, (conv, lin) in enumerate(zip(self.convs, self.lins)):
             x = conv(x, edge_index) + lin(x)
             if i != len(self.convs) - 1:
@@ -54,27 +59,14 @@ def multiagent_forward(net, sample):
     """ Returns logprobs and values for each agent. """
     # Run separate policies for each agent to calculate corresponding logits and values
     # Run REINFORCE with baseline
-    logits_per_agent = []
-    value_estimates_per_agent = []
-
-    for agent_id in range(n_agents):
-        # update x_dict to have current agent flag set appropriately
-        new_x_dict = sample.x_dict
-        new_x_dict['agent'] = new_x_dict['agent'].clone()
-        new_x_dict['agent'][agent_id, -1] = 1
-
-        out = net(new_x_dict, sample.edge_index_dict)
-
-        value_estimates_per_agent.append(
-            out['agent'][agent_id, 0]
-        )
-        logits_per_agent.append(
-            out['task'][:, 0]
-        )
-    
-    logprobs_per_agent = torch.stack(logits_per_agent).log_softmax(dim=-1)
-
-    return logprobs_per_agent, value_estimates_per_agent
+    out = net(sample.x_dict, sample.edge_index_dict)
+    agent = out['agent']
+    agent_norm = agent / torch.linalg.norm(agent, dim=1, keepdim=True)
+    task = out['task']
+    task_norm = task / torch.linalg.norm(task, dim=1, keepdim=True)
+    logits_per_agent = torch.matmul(agent_norm, task_norm.T)
+    logprobs_per_agent = F.log_softmax(logits_per_agent, dim=1)
+    return logprobs_per_agent, agent, task
 
 def sample_actions(logprobs_table: torch.Tensor, temperature=0.1):
     """
@@ -102,11 +94,11 @@ def main():
 
     for idx in tqdm.tqdm(range(n_scenarios), desc='Generating scenarios'):
         agent_locations, task_locations, task_assignment = generate_scenario(n_agents, n_tasks, width, height)
-        data.append((create_heterodata(width, height, agent_locations, task_locations), agent_locations, task_locations, task_assignment))
+        data.append((create_heterodata(width, height, agent_locations, task_locations, allow_agent_to_task=False), agent_locations, task_locations, task_assignment))
 
     dummy = data[0][0]
 
-    net = GNN([]) # 64, 64])
+    net = GNN([64])
     net = gnn.to_hetero(net, dummy.metadata(), aggr='sum')
 
     # populate the channel sizes by passing in a dummy dataset of the same shape
@@ -127,7 +119,7 @@ def main():
 
     if not skip:
         temperature = 0.1
-        epochs = 2
+        epochs = 10
         initial_lr = 1e-4
         end_lr = 1e-6
 
@@ -154,32 +146,67 @@ def main():
         lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optim, T_max=n_scenarios * epochs, eta_min=end_lr)
         for ep in range(epochs):
             for step, (sample, agent_locations, task_locations, task_assignment) in (pbar := tqdm.tqdm(enumerate(train), desc=f'Epoch {ep}')):
-                logprobs_per_agent, value_estimates_per_agent = multiagent_forward(net, sample)
+                logprobs_per_agent, agent, task = multiagent_forward(net, sample)
                 choices_per_agent = sample_actions(logprobs_per_agent, temperature)
-
                 reward_per_agent: list[float] = reward_functions.evaluate_assignment(choices_per_agent, agent_locations, task_locations) # type: ignore
-                value_loss = F.mse_loss(torch.stack(value_estimates_per_agent), torch.tensor(reward_per_agent))
-                chosen_logprobs_tensor = logprobs_per_agent[range(n_agents), choices_per_agent]
-                delta = (
-                    torch.tensor(reward_per_agent)
-                ).detach()
-                policy_loss = (-chosen_logprobs_tensor * delta).mean()
+
+                use_rl = True
+                if use_rl:
+                    # value_loss = F.mse_loss(torch.stack(value_estimates_per_agent), torch.tensor(reward_per_agent))
+                    chosen_logprobs_tensor = logprobs_per_agent[np.arange(n_agents), choices_per_agent]
+                    delta = (
+                        torch.tensor(reward_per_agent)
+                    ).detach()
+                    # delta = delta - delta.mean()
+                    # delta = delta / (delta.std() + 1e-8)
+                    loss = -(chosen_logprobs_tensor * delta).mean()
+                else:
+                    loss = nn.functional.cross_entropy(logprobs_per_agent, torch.tensor(task_assignment))
+
+                # plot task embedding tsne
 
                 # visualize reward per agent
                 if step % 1000 == 0:
                     reward_grid = np.zeros((n_agents, n_tasks)) - 100
-                    for i, choice in enumerate(choices_per_agent):
-                        reward_grid[i, choice] = reward_per_agent[i]
+                    reward_grid[np.arange(n_agents), choices_per_agent] = reward_per_agent
                     plt.clf()
-                    plt.imshow(reward_grid)
+
+                    plt.subplot(1, 3, 1)
+                    plt.imshow(reward_grid[:, task_assignment])
                     plt.colorbar()
                     plt.title("Reward per agent: Step " + str(step))
                     plt.xlabel("Task")
                     plt.ylabel("Agent")
-                    plt.pause(1)
-                
-                loss = value_loss + policy_loss
 
+                    plt.subplot(1, 3, 2)
+                    plt.imshow(logprobs_per_agent[:, task_assignment].detach().numpy())
+                    plt.colorbar()
+                    plt.title("Logprobs per agent: Step " + str(step))
+                    plt.xlabel("Task")
+                    plt.ylabel("Agent")
+                    plt.tight_layout()
+
+                    # print task embedding variance
+                    print('===')
+                    print("task embedding std. dev:")
+                    print(task.std(dim=0).mean())
+                    print("task embedding magnitudes:")
+                    print(task.mean(dim=0))
+                    print('===')
+
+                    # plt.subplot(1, 3, 3)
+                    # tsne = sklearn.manifold.TSNE(n_components=2, perplexity=5)
+                    # task_embedding = task.detach().numpy()
+                    # task_embedding = tsne.fit_transform(task_embedding)
+                    # task_colors = np.zeros((n_tasks, 3))
+                    # task_colors[:, 0] = task_locations[:, 0]/width
+                    # task_colors[:, 1] = task_locations[:, 1]/height
+                    # task_colors[:, 2] = 0
+                    # plt.title("Task embedding t-SNE")
+                    # plt.scatter(task_embedding[:, 0], task_embedding[:, 1], c=task_colors)
+
+                    plt.pause(1)
+                    
                 optim.zero_grad()
                 loss.backward()
                 optim.step()
@@ -234,7 +261,7 @@ def main():
         for i, (sample, agent_locations, task_locations, task_assignment) in (pbar := tqdm.tqdm(enumerate(test), desc='Evaluating...')):
             display = (i == 0) and True
 
-            logprobs_per_agent, value_estimates_per_agent = multiagent_forward(net, sample)
+            logprobs_per_agent, agent, task = multiagent_forward(net, sample)
 
             greedy_eval_value, true_value, random_value = visualize.evaluate(logprobs_per_agent, agent_locations, task_locations, task_assignment, display=display)
             greedys.append(greedy_eval_value)
@@ -245,10 +272,6 @@ def main():
     print("mean true:", trues.mean())
     print("std true:", trues.std())
 
-    softs = np.array(softs)
-    print("mean soft:", softs.mean())
-    print("std soft:", softs.std())
-
     greedys = np.array(greedys)
     print("mean greedy:", greedys.mean())
     print("std greedy:", greedys.std())
@@ -258,4 +281,7 @@ def main():
     print("std random:", randoms.std())
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    finally:
+        plt.close()